6f06f3bede665e8bfaa3232dcca4ecfc2bac055b
ApollonUmets <123029502+ApollonUmets@users.noreply.github.com>
2023-02-04 23:00:36 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner text = new Scanner(System.in);
+        System.out.println("Введите текст :");
+
+        String a = text.nextLine();
+
+        System.out.println(" Введите слово которое желаете вырезать :");
+        String word = text.nextLine();
+
+        int start = a.indexOf(word);
+        String befor = a.substring(0,start);
+
+        String after = a.substring(start + word.length());
+        String result = befor + after;
+
+        System.out.println(result);
+
+
+
+    }
+}
